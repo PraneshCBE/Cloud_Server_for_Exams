7809af49aae1c2b1fd0b8743f1eed48118c35489
pr <rvpspran@gmail.com>
2023-09-26 09:58:56 +0530

    pwd

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const mysql = require('mysql');
 const db = mysql.createConnection({
   host: 'localhost', 
   user: 'root', 
-  password: 'Pranesh@123', 
+  password: '', 
   database: 'login_db', 
 });
 
