ccb0ae654802c04f29270e5f86686f90c131462c
pr <rvpspran@gmail.com>
2023-10-09 14:11:00 +0530

    midterm

--- server.js
+++ server.js
@@ -1,76 +1,89 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
 const path = require('path');
-const { parse } = require('querystring');
-const mysql = require('mysql'); 
 
+const app = express();
+const port = 3000;
+
+// Create a MySQL connection
 const db = mysql.createConnection({
-  host: 'localhost', 
-  user: 'root', 
-  password: 'MyNewPass1!', 
-  database: 'login_db', 
+  host: '35.184.0.60',
+  user: 'root',
+  password: '12345678',
+  database: 'logreg',
 });
 
 // Connect to MySQL
 db.connect((err) => {
   if (err) {
-    console.error('Error connecting to MySQL:', err);
-    return;
+    throw err;
   }
   console.log('Connected to MySQL');
 });
 
-const server = http.createServer((req, res) => {
-  if (req.method === 'GET' && req.url === '/') {
-    // Serve the HTML form
-    const filePath = path.join(__dirname, 'index.html');
-    fs.readFile(filePath, 'utf8', (err, data) => {
-      if (err) {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Internal Server Error');
-        return;
-      }
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(data);
-    });
-  } else if (req.method === 'POST' && req.url === '/login') {
-    // Handle form submission
-    let body = '';
-    req.on('data', (chunk) => {
-      body += chunk.toString();
-    });
-    req.on('end', () => {
-      const formData = parse(body);
-      const username = formData.username;
-      const password = formData.password;
+// Middleware for parsing JSON and form data
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-      // Validate the username and password against the MySQL database
-      const query = 'SELECT * FROM users WHERE username = ? AND password = ?';
-      db.query(query, [username, password], (err, results) => {
-        if (err) {
-          console.error('Error querying MySQL:', err);
-          res.writeHead(500, { 'Content-Type': 'text/plain' });
-          res.end('Internal Server Error');
-        } else if (results.length === 1) {
-          // Valid credentials
-          res.writeHead(200, { 'Content-Type': 'text/plain' });
-          res.end('Congratulations! Login successful.');
-        } else {
-          // Invalid credentials
-          res.writeHead(401, { 'Content-Type': 'text/plain' });
-          res.end('Invalid username or password.');
-        }
-      });
-    });
-  } else {
-    // Handle other requests (e.g., 404)
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found');
-  }
+// Serve the HTML file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-const port = 3000;
+// Handle registration POST request
+app.post('/register', (req, res) => {
+  const { username, email, password, name } = req.body;
 
-server.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  // Hash and salt the password (you should use a library like bcrypt)
+  // Insert the user data into the MySQL database
+  const insertQuery = `INSERT INTO users (username, email, password, name) VALUES (?, ?, ?, ?)`;
+  db.query(insertQuery, [username, email, password, name], (err, result) => {
+    if (err) {
+      res.status(500).send('Registration failed');
+      console.log(err);
+    } else {
+      const successHTML = `
+        <html>
+        <head>
+            <title>Registration Successful</title>
+        </head>
+        <body>
+            <h2>Registration Successful</h2>
+            <p>Thank you for registering!</p>
+            <p>Your details:</p>
+            <ul>
+                <li><strong>Name:</strong> ${name}</li>
+                <li><strong>Username:</strong> ${username}</li>
+                <li><strong>Email:</strong> ${email}</li>
+            </ul>
+        </body>
+        </html>
+      `;
+      res.send(successHTML);
+    }
+  });
+});
+
+// Handle login POST request
+app.post('/login', (req, res) => {
+  const { login_username, login_password } = req.body;
+
+  // Query the MySQL database to check login credentials
+  const selectQuery = `SELECT * FROM users WHERE username = ? AND password = ?`;
+  db.query(selectQuery, [login_username, login_password], (err, result) => {
+    if (err) {
+      res.status(500).json({ message: 'Login failed' });
+    } else {
+      if (result.length === 1) {
+        res.send('Login successful');
+      } else {
+        res.status(401).json({ message: 'Login failed' });
+      }
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
